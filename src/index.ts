b7afa75c4119f4dc2e4f3d88ad619ba736b72001
郭斌勇 <guobinyong@qq.com>
2020-06-01 19:39:41 +0800

    完成基础版customJSONStringify

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,54 @@
-import {getTypeStringOf} from "com-tools"
+import {getExactTypeStringOf, ExactType, ExactTypeString, getStringOfType} from "com-tools"
 
 
 type Reviver = (this: any, key: string,value: any) => any;
-type DataType = any;
 
-type TypeReviverArray = [DataType,Reviver][];
+
+type DataType =  ExactTypeString | ExactType;
+type DataTypeArray = DataType[];
+type Types = DataType | DataTypeArray;
+
+
+type TypeReviverArray = [Types,Reviver][];
+
+type TypeStringReviverArray = [ExactTypeString,Reviver][];
+
+
+/**
+ * 扁平化的 TypeReviverArray
+ */
+type TypeReviverFlatArray = [DataType,Reviver][];
+
 type TypeReviverObject = {
     [typeName:string]:Reviver
 };
-type TypeReviverMap = Map<DataType,Reviver>;
+type TypeReviverMap = Map<Types,Reviver>;
 
-type TypeReviver = TypeReviverArray | TypeReviverObject | TypeReviverMap;
+/**
+ * 扁平化的 TypeReviverMap
+ */
+type TypeReviverFlatMap = Map<DataType,Reviver>;
+
+type TypeRevivers = TypeReviverArray | TypeReviverObject | TypeReviverMap;
 
 
+/**
+ * 将 typeRevivers 转成 TypeReviverArray
+ * @param typeRevivers
+ */
+function toTypeReviverArray(typeRevivers:TypeRevivers):TypeReviverArray {
 
-function toTypeReviverArray(typeReviver:TypeReviver):TypeReviverArray {
-
-    switch (typeReviver.constructor.name) {
+    switch (typeRevivers.constructor.name) {
         case "Map":{
-            var typeRevArr = Array.from(typeReviver as TypeReviverMap);
+            var typeRevArr = Array.from(typeRevivers as TypeReviverMap);
             break;
         }
         case "Array":{
-            typeRevArr = typeReviver as TypeReviverArray;
+            typeRevArr = typeRevivers as TypeReviverArray;
             break;
         }
         default:{
-            typeRevArr = Object.entries(typeReviver);
+            typeRevArr = Object.entries(typeRevivers);
         }
     }
 
@@ -34,18 +56,88 @@ function toTypeReviverArray(typeReviver:TypeReviver):TypeReviverArray {
 }
 
 
-function toTypeReviverMap(typeReviver:TypeReviver):TypeReviverMap {
-    switch (typeReviver.constructor.name) {
+
+
+function toTypeReviverObject(typeRevivers:TypeRevivers):TypeReviverObject  {
+    let typeReviverArr = toTypeReviverArray(typeRevivers);
+
+    let typeStrReviverArr:TypeStringReviverArray = typeReviverArr.reduce(function (flatArr:TypeStringReviverArray,typeReviver) {
+        let types = typeReviver[0];
+        let reviver = typeReviver[1];
+
+        let typeArr:DataTypeArray = Array.isArray(types) ? types : [types];
+
+        typeArr.forEach(function (dataType) {
+            flatArr.push([typeof dataType === "string" ? dataType : getStringOfType(dataType),reviver]);
+        });
+
+        return flatArr;
+
+    },[]);
+
+    return  Object.fromEntries(typeStrReviverArr);
+}
+
+
+
+
+function customJSONStringify(value: any, typeRevivers:TypeRevivers,typeMark:string = "[type]:",selfCall?:boolean):string {
+    let trObj:TypeReviverObject = selfCall ? typeRevivers as TypeReviverObject : toTypeReviverObject(typeRevivers);
+
+    function stringifyReviver(this: any, key: string,value: any) {
+        let typeStr = getExactTypeStringOf(value);
+        var revier = trObj[typeStr];
+        if (!revier){
+            return value;
+        }
+
+        let rerRes = revier.call(this,key,value);
+
+        if (rerRes === undefined){
+            return rerRes;
+        }
+
+        let rerStr:string = typeof rerRes === "string" ? rerRes : customJSONStringify(value,trObj,typeMark,true);
+        let typePrefix = typeMark.replace(/type/i,typeStr);
+
+        return typePrefix + rerStr;
+    }
+
+    return  JSON.stringify(value,stringifyReviver);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//用不到的工具-----------------
+
+
+
+
+/**
+ * 将 typeRevivers 转成 TypeReviverMap
+ * @param typeRevivers
+ */
+function toTypeReviverMap(typeRevivers:TypeRevivers):TypeReviverMap {
+    switch (typeRevivers.constructor.name) {
         case "Map":{
-            var typeRevMap = typeReviver as TypeReviverMap;
+            var typeRevMap = typeRevivers as TypeReviverMap;
             break;
         }
         case "Array":{
-            typeRevMap = new Map(typeReviver as TypeReviverArray);
+            typeRevMap = new Map(typeRevivers as TypeReviverArray);
             break;
         }
         default:{
-            typeRevMap = new Map(Object.entries(typeReviver));
+            typeRevMap = new Map(Object.entries(typeRevivers));
         }
     }
 
@@ -55,15 +147,48 @@ function toTypeReviverMap(typeReviver:TypeReviver):TypeReviverMap {
 
 
 
+/**
+ * 扁平化 并扩展 TypeReviverArray
+ * 扁平化 会把 TypeReviverArray 中 [DataTypeArray,Reviver] 映射关系 改成  多个 [DataType,Reviver] 的映射关系
+ * 扩展 会把 给 TypeReviverArray 增给第个 TypeReviver 增加一个对应  [DataType的名字,Reviver]，DataType的名字 就是 DataType 的字符串表示
+ * @param TypeReviverFlatArray
+ */
+function flatExtendTypeReviverArray(typeReviverArr:TypeReviverArray):TypeReviverFlatArray {
+    return  typeReviverArr.reduce(function (flatArr:TypeReviverFlatArray,typeReviver) {
+        let types = typeReviver[0];
+        let reviver = typeReviver[1];
 
-function customStringify(value: any, typeReviver:TypeReviver) {
-    let typeRevMap = toTypeReviverMap(typeReviver);
+        let typeArr:DataTypeArray = Array.isArray(types) ? types : [types];
 
-    function stringifyReviver(this: any, key: string,value: any) {
+        typeArr.forEach(function (dataType) {
+            flatArr.push([dataType,reviver]);
+            if (typeof dataType !== "string"){
+                let typeStr = getStringOfType(dataType);
+                flatArr.push([typeStr,reviver])
+            }
+        });
+
+        return flatArr;
+
+    },[] as TypeReviverFlatArray);
+}
 
 
-    }
+/**
+ * 扁平化 并扩展 TypeRevivers
+ * @param typeRevivers
+ */
+function flatExtendTypeRevivers(typeRevivers:TypeRevivers):TypeReviverFlatArray {
+    let trArr = toTypeReviverArray(typeRevivers);
+    return flatExtendTypeReviverArray(trArr);
+}
 
 
-
+/**
+ * 将 typeRevivers 扁平化并扩展成 TypeReviverFlatMap
+ * @param typeRevivers
+ */
+function flatExtendToTypeReviverFlatMap(typeRevivers:TypeRevivers):TypeReviverFlatMap {
+    let flatArr = flatExtendTypeRevivers(typeRevivers);
+    return  new Map(flatArr);
 }
