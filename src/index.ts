c0e2131f7fe684abe25b8d5d40684b7315b9e955
郭斌勇 <guobinyong@qq.com>
2020-06-09 11:33:16 +0800

    抽离revivers为独立的包

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,201 @@
-export * from "./customJSON"
-export * from "./deepCopy"
-export * from "./revivers"
+import type {StringifyReviverOptions,ReviverPair} from "json-tls";
+import type {TypeReviverArray} from "type-reviver"
+
+
+// Date ----------
+
+//原始的 toJSON 方法
+const Date_toJSON = Date.prototype.toJSON
+
+//StringifyReviver
+export function Date_StringifyReviver(key: string,value: any,type:string ,callCount:number,stringifyOptions:StringifyReviverOptions) {
+    if(stringifyOptions.skip || stringifyOptions.skipMark || (callCount === 1 && stringifyOptions.skipRootMark)){
+        return Date_toJSON ? Date_toJSON.call(value) : value.toString();
+    }
+    return value.valueOf();
+}
+
+//ParseReviver
+export function Date_ParseReviver(key: string,value: any,type:string,callCount:number) {
+    return new Date(value);
+}
+
+
+
+
+
+// Map ----------
+
+
+
+//原始的 toJSON 方法
+
+// @ts-ignore
+const Map_toJSON = Map.prototype.toJSON
+
+//StringifyReviver
+export function Map_StringifyReviver(key: string,value: any,type:string ,callCount:number,stringifyOptions:StringifyReviverOptions) {
+    if(Map_toJSON && (stringifyOptions.skip || stringifyOptions.skipMark || (callCount === 1 && stringifyOptions.skipRootMark))){
+        return Map_toJSON.call(value);
+    }
+    return Array.from(value);
+}
+
+//ParseReviver
+export function Map_ParseReviver(key: string,value: any,type:string,callCount:number) {
+    return new Map(value);
+}
+
+
+
+
+
+
+// Set ----------
+
+
+
+//原始的 toJSON 方法
+
+// @ts-ignore
+const Set_toJSON = Set.prototype.toJSON
+
+//StringifyReviver
+export function Set_StringifyReviver(key: string,value: any,type:string ,callCount:number,stringifyOptions:StringifyReviverOptions) {
+    if(Set_toJSON && (stringifyOptions.skip || stringifyOptions.skipMark || (callCount === 1 && stringifyOptions.skipRootMark))){
+        return Set_toJSON.call(value);
+    }
+    return Array.from(value);
+}
+
+//ParseReviver
+export function Set_ParseReviver(key: string,value: any,type:string,callCount:number) {
+    return new Set(value);
+}
+
+
+
+
+
+
+
+// URL ----------
+
+
+
+//原始的 toJSON 方法
+
+// @ts-ignore
+const URL_toJSON = URL.prototype.toJSON
+
+//StringifyReviver
+export function URL_StringifyReviver(key: string,value: any,type:string ,callCount:number,stringifyOptions:StringifyReviverOptions) {
+    if(URL_toJSON && (stringifyOptions.skip || stringifyOptions.skipMark || (callCount === 1 && stringifyOptions.skipRootMark))){
+        return URL_toJSON.call(value);
+    }
+    return value.href;
+}
+
+//ParseReviver
+export function URL_ParseReviver(key: string,value: any,type:string,callCount:number) {
+    return new URL(value);
+}
+
+
+
+
+
+
+// RegExp ----------
+
+
+
+//原始的 toJSON 方法
+
+// @ts-ignore
+const RegExp_toJSON = RegExp.prototype.toJSON
+
+//StringifyReviver
+export function RegExp_StringifyReviver(key: string,value:any,type:string ,callCount:number,stringifyOptions:StringifyReviverOptions) {
+    if(stringifyOptions.skip || stringifyOptions.skipMark || (callCount === 1 && stringifyOptions.skipRootMark)){
+        return RegExp_toJSON ? RegExp_toJSON.call(value) : value.toString();
+    }
+
+    return {
+        source:value.source,
+        flags:value.flags
+    };
+}
+
+//ParseReviver
+export function RegExp_ParseReviver(key: string,value: any,type:string,callCount:number) {
+    let {source,flags} = value;
+    let reg = new RegExp(source,flags);
+    return reg;
+}
+
+
+
+
+
+// Function ----------
+
+
+
+//原始的 toJSON 方法
+
+// @ts-ignore
+const Function_toJSON = Function.prototype.toJSON
+
+//StringifyReviver
+export function Function_StringifyReviver(key: string,value:any,type:string ,callCount:number,stringifyOptions:StringifyReviverOptions) {
+    if(Function_toJSON && stringifyOptions.skip || stringifyOptions.skipMark || (callCount === 1 && stringifyOptions.skipRootMark)){
+        return Function_toJSON.call(value);
+    }
+    return {
+        source:value.toString(),
+        name:value.name
+    };
+}
+
+//ParseReviver
+export function Function_ParseReviver(key: string,value: any,type:string,callCount:number) {
+    /*
+    考虑到安全和性能的原因，弃用 eval，改用 Function；
+    */
+    let {source,name} = value;
+    //判断函数代码是否是匿名函数
+    let hasName = /function\s+[A-Za-z_$]+[\w$]*\s*\(/.test(source);
+
+    if (!hasName && /^[A-Za-z_$]+[\w$]*$/.test(name)){ // 当 函数代码是匿名函数 且 name 是有效的标识符
+        var funBody = `"use strict"; var ${name} = ${source} ; return ${name}`;
+    }else {
+        funBody = `"use strict"; return (${source})`;
+    }
+    return (new Function(funBody))();
+
+    /*
+    匿名函数 不能用于 函数声明，只能用于函数表达式；
+    所以，如果 value 是匿名函数，则 `eval(value);` 会报错： `Uncaught SyntaxError: Function statements require a function name`
+    此时，只需要用 `()` 将其转为 函数表达式 即可；
+    */
+    // return eval(`(${value})`);
+
+}
+
+
+
+
+
+
+
+export const typeReviverArray:TypeReviverArray<ReviverPair> = [
+    [Date,{string:Date_StringifyReviver, parse:Date_ParseReviver}],
+    [Map,{string:Map_StringifyReviver, parse:Map_ParseReviver}],
+    [Set,{string:Set_StringifyReviver, parse:Set_ParseReviver}],
+    [URL,{string:URL_StringifyReviver, parse:URL_ParseReviver}],
+    [RegExp,{string:RegExp_StringifyReviver, parse:RegExp_ParseReviver}],
+    [Function,{string:Function_StringifyReviver, parse:Function_ParseReviver}],
+];
+
+export default typeReviverArray;
